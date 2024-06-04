294f0f7b628b681b1aee4dc8fdde928840552353
Haoqi Shi <haoqi.shi@xyzrobotics.com>
2024-06-04 15:44:47 +0800

    add remove_points_near

---
+++ src/bin/remove_points_near.rs
@@ -0,0 +1,244 @@
+use clap::Parser;
+use ncollide3d::bounding_volume::{bounding_sphere, BoundingSphere, BoundingVolume};
+use ncollide3d::na;
+use ncollide3d::partitioning::{BVH, BVT};
+use ncollide3d::query::visitors::PointInterferencesCollector;
+use ncollide3d::query::PointQuery;
+use ncollide3d::shape::{Cuboid, TriMesh};
+use serde::{Deserialize, Serialize};
+use std::io::{BufRead, Read, Write};
+use std::vec;
+use stl_io::read_stl;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct CubeSerde {
+    tf: Vec<Vec<f64>>,
+    size: Vec<f64>,
+}
+
+#[derive(Debug)]
+struct CuboidWithTf {
+    cuboid: Cuboid<f64>,
+    tf: na::Isometry3<f64>,
+}
+
+#[derive(Parser, Debug)]
+struct Cli {
+    #[arg(short, long)]
+    input_dir: String,
+
+    #[arg(short, long, default_value = "0.01")]
+    distance: f64,
+
+    #[arg(short, long)]
+    output_dir: String,
+}
+
+fn load_from_directory(
+    dir: &str,
+) -> (
+    Vec<CuboidWithTf>,
+    Option<TriMesh<f64>>,
+    Vec<na::Point3<f64>>,
+) {
+    let mut json_files: Vec<String> = vec![];
+    let mut stl_files: Vec<String> = vec![];
+    let mut pcd_files: Vec<String> = vec![];
+    let paths = std::fs::read_dir(dir).unwrap();
+    for path in paths {
+        let path = path.unwrap().path();
+        let path_str = path.to_str().unwrap();
+        if path_str.ends_with(".json") {
+            json_files.push(path_str.to_string());
+        } else if path_str.ends_with(".stl") {
+            stl_files.push(path_str.to_string());
+        } else if path_str.ends_with(".pcd") {
+            pcd_files.push(path_str.to_string());
+        }
+    }
+
+    if json_files.len() != 1 {
+        panic!("There must be exactly 2 JSON files in the input directory");
+    }
+
+    let cube_serdes = serde_json::from_str::<Vec<CubeSerde>>(
+        &std::fs::read_to_string(&json_files[0]).expect("Failed to read JSON file"),
+    )
+    .unwrap();
+
+    let cubes: Vec<CuboidWithTf> = cube_serdes
+        .iter()
+        .map(|t| {
+            let rotmat = na::Rotation3::from_matrix_unchecked(na::Matrix3::from_columns(&[
+                na::Vector3::new(t.tf[0][0], t.tf[0][1], t.tf[0][2]),
+                na::Vector3::new(t.tf[1][0], t.tf[1][1], t.tf[1][2]),
+                na::Vector3::new(t.tf[2][0], t.tf[2][1], t.tf[2][2]),
+            ]));
+            let translation = na::Translation3::new(t.tf[0][3], t.tf[1][3], t.tf[2][3]);
+            let tf = na::Isometry3::from_parts(translation, rotmat.into());
+            let size = na::Vector3::new(t.size[0], t.size[1], t.size[2]);
+            CuboidWithTf {
+                cuboid: Cuboid::new(size / 2.0),
+                tf,
+            }
+        })
+        .collect();
+
+    let mut pc: Vec<na::Point3<f64>> = vec![];
+    if pcd_files.len() == 1 {
+        let file = std::fs::File::open(&pcd_files[0]).unwrap();
+        let mut reader = std::io::BufReader::new(file);
+        let mut has_normal = false;
+        // read until the end of the header
+        loop {
+            let mut line = String::new();
+            reader.read_line(&mut line).unwrap();
+            if line.trim().contains("normal_x") {
+                has_normal = true;
+            }
+            if line.trim() == "DATA binary" {
+                break;
+            }
+        }
+        // read the binary data
+        loop {
+            match has_normal {
+                true => {
+                    let mut buf = [0u8; 4 * 6];
+                    match reader.read_exact(&mut buf) {
+                        Ok(_) => {
+                            let x = f32::from_le_bytes([buf[0], buf[1], buf[2], buf[3]]);
+                            let y = f32::from_le_bytes([buf[4], buf[5], buf[6], buf[7]]);
+                            let z = f32::from_le_bytes([buf[8], buf[9], buf[10], buf[11]]);
+                            pc.push(na::Point3::new(x as f64, y as f64, z as f64));
+                        }
+                        Err(_) => break,
+                    }
+                }
+                false => {
+                    let mut buf = [0u8; 4 * 3];
+                    match reader.read_exact(&mut buf) {
+                        Ok(_) => {
+                            let x = f32::from_le_bytes([buf[0], buf[1], buf[2], buf[3]]);
+                            let y = f32::from_le_bytes([buf[4], buf[5], buf[6], buf[7]]);
+                            let z = f32::from_le_bytes([buf[8], buf[9], buf[10], buf[11]]);
+                            pc.push(na::Point3::new(x as f64, y as f64, z as f64));
+                        }
+                        Err(_) => break,
+                    }
+                }
+            }
+        }
+    }
+
+    if stl_files.len() == 1 {
+        let file = std::fs::File::open(&stl_files[0]).unwrap();
+        let mut reader = std::io::BufReader::new(file);
+        let stl = read_stl(&mut reader).expect("Failed to read STL file");
+
+        let vertices = stl
+            .vertices
+            .iter()
+            .map(|v| na::Point3::new(v[0] as f64, v[1] as f64, v[2] as f64))
+            .collect();
+        let indices = stl
+            .faces
+            .iter()
+            .map(|f| na::Point3::new(f.vertices[0], f.vertices[1], f.vertices[2]))
+            .collect();
+
+        let scene_mesh = TriMesh::new(vertices, indices, None);
+        (cubes, Some(scene_mesh), pc)
+    } else {
+        (cubes, None, pc)
+    }
+}
+
+fn write_to_pcd(points: &Vec<na::Point3<f64>>, path: &str) {
+    let mut file = std::fs::File::create(path).unwrap();
+    file.write_all(b"VERSION .7\n").unwrap();
+    file.write_all(b"FIELDS x y z\n").unwrap();
+    file.write_all(b"SIZE 4 4 4\n").unwrap();
+    file.write_all(b"TYPE F F F\n").unwrap();
+    file.write_all(b"COUNT 1 1 1\n").unwrap();
+    file.write_all(b"WIDTH ").unwrap();
+    file.write_all(format!("{}\n", points.len()).as_bytes())
+        .unwrap();
+    file.write_all(b"HEIGHT 1\n").unwrap();
+    file.write_all(b"VIEWPOINT 0 0 0 1 0 0 0\n").unwrap();
+    file.write_all(b"POINTS ").unwrap();
+    file.write_all(format!("{}\n", points.len()).as_bytes())
+        .unwrap();
+    file.write_all(b"DATA binary\n").unwrap();
+    for i in 0..points.len() {
+        file.write_all(&(points[i].coords.x as f32).to_le_bytes())
+            .unwrap();
+        file.write_all(&(points[i].coords.y as f32).to_le_bytes())
+            .unwrap();
+        file.write_all(&(points[i].coords.z as f32).to_le_bytes())
+            .unwrap();
+    }
+}
+
+fn remove_points_near(
+    cubes: &Vec<CuboidWithTf>,
+    scene_mesh: &Option<TriMesh<f64>>,
+    pc: &Vec<na::Point3<f64>>,
+    margin: f64,
+) -> (Vec<na::Point3<f64>>, Vec<na::Point3<f64>>) {
+    let mut points_to_keep: Vec<na::Point3<f64>> = vec![];
+    let mut points_to_remove: Vec<na::Point3<f64>> = vec![];
+    let mut leaves: Vec<(usize, BoundingSphere<f64>)> = vec![];
+    for (index, cube) in cubes.iter().enumerate() {
+        let mut bs = bounding_sphere(&cube.cuboid, &cube.tf);
+        bs.loosen(margin);
+        leaves.push((index, bs));
+    }
+    let bvt = BVT::new_balanced(leaves.clone());
+    for point in pc.iter() {
+        let mut interferences: Vec<usize> = Vec::new();
+        let mut visitor = PointInterferencesCollector::new(point, &mut interferences);
+        bvt.visit(&mut visitor);
+
+        let mut removed = false;
+        for i in interferences.iter() {
+            let cube = &cubes[*i];
+            let distance = cube.cuboid.distance_to_point(&cube.tf, &point, true);
+            if distance < margin {
+                points_to_remove.push(*point);
+                removed = true;
+                break;
+            }
+        }
+        if removed {
+            continue;
+        }
+
+        if let Some(scene_mesh) = scene_mesh {
+            let distance = scene_mesh.distance_to_point(&na::Isometry3::identity(), &point, true);
+            if distance < margin {
+                points_to_remove.push(*point);
+                continue;
+            }
+        }
+
+        points_to_keep.push(*point);
+    }
+
+    (points_to_keep, points_to_remove)
+}
+
+fn main() {
+    let args = Cli::parse();
+    let (cubes, scene_mesh, pc) = load_from_directory(&args.input_dir);
+    let (points_to_keep, points_to_remove) =
+        remove_points_near(&cubes, &scene_mesh, &pc, args.distance);
+    write_to_pcd(
+        &points_to_keep,
+        &format!("{}/points_to_keep.pcd", args.output_dir),
+    );
+    write_to_pcd(
+        &points_to_remove,
+        &format!("{}/points_to_remove.pcd", args.output_dir),
+    );
+}
