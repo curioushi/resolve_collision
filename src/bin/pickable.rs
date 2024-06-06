2833e136a82b9a12c1d643354fc80a821efd0e2c
curioushi <curioushi@163.com>
2024-06-06 23:11:35 +0800

    add align axis

--- src/bin/pickable.rs
+++ src/bin/pickable.rs
@@ -48,70 +48,84 @@ fn load_from_directory(dir: &str) -> Vec<CuboidWithTf> {
     cubes
 }
 
+fn align_axis(cube: &mut CuboidWithTf) {
+    // find Z axis
+    let mut candidates = vec![];
+    let mut temp = cube.clone();
+    candidates.push(temp.clone());
+    temp.rot_x_90();
+    candidates.push(temp.clone());
+    temp.rot_x_90();
+    candidates.push(temp.clone());
+    temp.rot_x_90();
+    candidates.push(temp.clone());
+    temp.rot_x_90();
+    temp.rot_y_90();
+    candidates.push(temp.clone());
+    temp.rot_y_90();
+    temp.rot_y_90();
+    candidates.push(temp.clone());
+
+    let mut max_index = 0;
+    let mut max_value = f64::MIN;
+    for (i, c) in candidates.iter().enumerate() {
+        let matrix = c.tf.to_matrix();
+        let dot_z = matrix[(2, 2)];
+        if dot_z > max_value {
+            max_value = dot_z;
+            max_index = i;
+        }
+    }
+
+    temp = candidates[max_index].clone();
+
+    // find X axis
+    candidates.clear();
+    candidates.push(temp.clone());
+    temp.rot_z_90();
+    candidates.push(temp.clone());
+    temp.rot_z_90();
+    candidates.push(temp.clone());
+    temp.rot_z_90();
+    candidates.push(temp.clone());
+
+    let mut max_index = 0;
+    let mut max_value = f64::MIN;
+    for (i, c) in candidates.iter().enumerate() {
+        let matrix = c.tf.to_matrix();
+        let dot_x = matrix[(0, 0)];
+        if dot_x > max_value {
+            max_value = dot_x;
+            max_index = i;
+        }
+    }
+
+    *cube = candidates[max_index].clone();
+}
+
 fn main() {
     let args = Cli::parse();
-    let cubes = load_from_directory(&args.input_dir);
-    let mut new_cubes: Vec<CuboidWithTf> = vec![];
-    for cube in cubes.iter() {
-        let mut temp = cube.clone();
-        new_cubes.push(temp.clone());
-        temp.rot_z_90();
-        new_cubes.push(temp.clone());
-        temp.rot_z_90();
-        new_cubes.push(temp.clone());
-        temp.rot_z_90();
-        new_cubes.push(temp.clone());
-        temp.rot_z_90();
+    let mut cubes = load_from_directory(&args.input_dir);
 
-        temp.rot_x_90();
-        new_cubes.push(temp.clone());
-        temp.rot_z_90();
-        new_cubes.push(temp.clone());
-        temp.rot_z_90();
-        new_cubes.push(temp.clone());
-        temp.rot_z_90();
-        new_cubes.push(temp.clone());
-        temp.rot_z_90();
-
-        temp.rot_x_90();
-        new_cubes.push(temp.clone());
-        temp.rot_z_90();
-        new_cubes.push(temp.clone());
-        temp.rot_z_90();
-        new_cubes.push(temp.clone());
-        temp.rot_z_90();
-        new_cubes.push(temp.clone());
-        temp.rot_z_90();
-
-        temp.rot_x_90();
-        new_cubes.push(temp.clone());
-        temp.rot_z_90();
-        new_cubes.push(temp.clone());
-        temp.rot_z_90();
-        new_cubes.push(temp.clone());
-        temp.rot_z_90();
-        new_cubes.push(temp.clone());
-        temp.rot_z_90();
-        temp.rot_x_90();
-
-        temp.rot_y_90();
-        new_cubes.push(temp.clone());
-        temp.rot_z_90();
-        new_cubes.push(temp.clone());
-        temp.rot_z_90();
-        new_cubes.push(temp.clone());
-        temp.rot_z_90();
-        new_cubes.push(temp.clone());
-        temp.rot_z_90();
-
-        temp.rot_y_90();
-        temp.rot_y_90();
-        new_cubes.push(temp.clone());
-        temp.rot_z_90();
-        new_cubes.push(temp.clone());
-        temp.rot_z_90();
-        new_cubes.push(temp.clone());
-        temp.rot_z_90();
-        new_cubes.push(temp.clone());
+    for cube in cubes.iter_mut() {
+        align_axis(cube);
     }
+
+    let cube_serdes = cubes
+        .iter()
+        .map(|c| {
+            let tf = c.tf.to_homogeneous();
+            let size = c.cuboid.half_extents * 2.0;
+            CubeSerde {
+                tf: tf
+                    .row_iter()
+                    .map(|row| row.iter().map(|x| *x).collect())
+                    .collect(),
+                size: size.iter().map(|x| *x).collect(),
+            }
+        })
+        .collect::<Vec<CubeSerde>>();
+
+    let json_data = serde_json::to_string(&cube_serdes).unwrap();
+    std::fs::write(format!("{}/output.json", args.output_dir), json_data).unwrap();
 }
